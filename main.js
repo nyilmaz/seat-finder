7b1dfc49325b4177d7a0d06cff938d46e7c228a4
nyilmaz <nyilmaz88@gmail.com>
2013-10-02 16:16:51 +0300

    Kamil Koc eklendi

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var $ = require('jquery');
 var pamukkale = require('./pkale.js');
 var nilufer = require('./nilufer.js');
+var kamilkoc = require('./kkoc.js');
 var Logger = require('./logger.js');
 var logger = new Logger('MAIN');
 
+//kamilkoc icin istanbul(dudullu): 1105, izmir: 1118
 var tarihler = ['11/10/2013', '12/10/2013'];
 
 for(var i = 0; i < 300; i++){
@@ -13,7 +15,11 @@ for(var i = 0; i < 300; i++){
 			$(tarihler).each(function(i, x){
 				nilufer.getEmptySeatFor(3400, 3500, x, '19:00');
 				pamukkale.getEmptySeatFor(3400, 3500, x, '19:00');
+				kamilkoc.getEmptySeatFor(1105, 1118, x);
 			});
-		}, i*8000);
+		}, i*15000);
 	}(i));
 }
+
+
+
